747d31a1f61dffb4e722cc038c489747f9eb30df
Alex Walhout <jawalhout>
2017-07-31 22:51:29 -0400

    got things squared away and started on installing gulp

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-//the listening port
+//the listening port. this command STARTS the server, and sets it to port 80 which is the default port for http
+//the function() is a callback intentionally left blank. it is the callback for the function: app.listen
+//read both for info on callbacks they are ESSENTIAL
+//https://developers.google.com/web/fundamentals/getting-started/primers/promises
+//http://javascriptissexy.com/understand-javascript-callback-functions-and-use-them/
 app.listen(80, function () {
   console.log('Listening on port 80')
 })
