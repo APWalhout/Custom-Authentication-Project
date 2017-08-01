c56bd5bd1c1049868d3141791ac36294ce0db573
Alex Walhout <jawalhout>
2017-07-31 21:50:15 -0400

    The initial set up of the home, app, and main files in the view and layout folders. Possibly app.js is configured as a server? I copy pasted the code so I don't know what it does.

---
+++ app.js
@@ -0,0 +1,17 @@
+//obtained from https://github.com/ericf/express-handlebars
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+//sets the viewing engine as being run with the handlebars pkg?
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//gets the / route and renders home for that route as requested by Michael
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+//the listening port
+app.listen(3000);
