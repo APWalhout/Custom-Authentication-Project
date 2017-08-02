6a4cbc4f68b76a934bb0e3cc35881c105226d864
Alex Walhout <jawalhout>
2017-08-01 20:07:59 -0400

    setting up the gulp file to compile from sass/styles.scss to public/css/styles.css. Possibly did it wrong

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-{\rtf1}
+//load in the pkgs this file will use
+var gulp = require ('gulp');
+var gulp-sass = require ('gulp-sass');
+
+//make a gulp task to copy a file to a folder
+//in theory this task compiles styles.scss into styles.css in another folder
+gulp.task('copy', function() {
+  gulp.src('sass/styles.scss').pipe(gulp.dest('public/css/styles.css'))
+});
