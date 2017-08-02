b468cba360c370a2eaa2678f8f8a70c7611883d5
Alex Walhout <jawalhout>
2017-08-01 20:37:21 -0400

    problems with using gulp command as pkg bulma has undefined var in helpers.sass, and had to comment out @import ...sass/bounce-animation cause it was abscent/unreadable. Also, changed the package.json dependencies to 'latest' in case old versions were the problem...they weren't.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 //load in the pkgs this file will use
 var gulp = require ('gulp');
-var gulp-sass = require ('gulp-sass');
+var gutil = require ('gulp-util');
+var sass = require ('gulp-sass');
 
 //make a gulp task to copy a file to a folder
 //in theory this task compiles styles.scss into styles.css in another folder
-gulp.task('copy', function() {
-  gulp.src('sass/styles.scss').pipe(gulp.dest('public/css/styles.css'))
+gulp.task('default', function() {
+  gulp.src('sass/styles.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('public/css'))
 });
