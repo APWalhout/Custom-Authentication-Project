0fe51abb018d8e30cb74662b99df25009d25ed3d
Alex Walhout <jawalhout>
2017-08-02 17:32:54 -0400

    improperly implementing gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 //load in the pkgs this file will use
 var gulp    = require ('gulp')
-  , sass    = require ('gulp-sass');
+  , sass    = require ('gulp-sass')
+  , connect = require ('gulp-connect');
 
 'use strict';
-
+//the command gulp default or just gulp now watches all files in folder sass with type .scss extension
+//and when they change it calls the 'sass' task which recompiles the files into one .css file in the public folder
 gulp.task('default', function () {
   gulp.watch('./sass/*.scss', ['sass'])
+  gulp.watch('./views/*.handlebars', ['handlebars'])
 });
 //make a gulp task to copy a file to a folder
 gulp.task('sass', function() {
@@ -13,3 +16,17 @@ gulp.task('sass', function() {
     .pipe(sass( { errLogToConsole: true } ))
     .pipe(gulp.dest('./public/css'))
 })
+//connect, html, default, and live are all to perform livereloads
+gulp.task('connect', function() {
+  connect.server({
+    root: 'views/layouts',
+    livereload: true
+  });
+});
+
+gulp.task('handlebars', function () {
+  gulp.src('./views/*.handlebars')
+    .pipe(connect.reload());
+});
+
+gulp.task('live', ['connect', 'default']);
